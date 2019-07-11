9ef5127dbdaed3bce45cea11351dccf31787e8bf
JosephPR <reilly4181@gmail.com>
2019-07-11 15:09:34 -0600

    fixed post route

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :pets, only: [:index, :show, :update, :destroy]
-      resources :dogs, only: [:index, :show, :update, :destroy]
-      resources :cats, only: [:index, :show, :update, :destroy]
-      resources :exotics, only: [:index, :show, :update, :destroy]
+      resources :dogs, only: [:index, :show, :create, :update, :destroy]
+      resources :cats, only: [:index, :show, :create, :update, :destroy]
+      resources :exotics, only: [:index, :show,:create, :update, :destroy]
     end
   end
 end
